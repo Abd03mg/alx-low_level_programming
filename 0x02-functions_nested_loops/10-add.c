79d022455ea6c42abf735e965b278728f9779306
Abd03mg <89263478+Abd03mg@users.noreply.github.com>
2022-12-17 19:11:29 +0200

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - function adds two int togather
+* @a,b - The two values they will be added
+* return - returns the added value
+*/
+
+int add(int a, int b)
+{
+  int res;
+  res = a + b;
+  return (res);
+}
