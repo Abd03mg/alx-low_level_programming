dd25a23827a8f104db4e97d3a840797657099b88
Abd03mg <89263478+Abd03mg@users.noreply.github.com>
2022-12-19 21:57:25 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,7 +15,7 @@ void print_rev(char *s)
         {
             
         }
-        for (j = i; s[j]>=0; j--)
+        for (j = i - 1; s[j]>=0; j--)
         {
             _putchar(s[j]);
         }
