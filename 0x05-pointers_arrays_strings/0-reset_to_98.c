6c079f2d98970edf0c7dc02a77077d331189b2af
Abd03mg <89263478+Abd03mg@users.noreply.github.com>
2022-12-19 19:08:08 +0200

    Create 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* reset_to_98 - Function that point value of n to 98.
+* @*n: the value that will assigned to 98.
+*/
+
+void reset_to_98(int *n)
+{
+  int a;
+  
+  a = 98;
+  n = &a;
+  *n = a 
+}
