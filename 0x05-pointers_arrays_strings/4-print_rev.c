e16dedcf30540417eca01d138b2c9a0bd7f6f809
Abd03mg <89263478+Abd03mg@users.noreply.github.com>
2022-12-19 21:46:43 +0200

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_rev - main function reverse string.
+* @s: input string.
+*/
+
+void print_rev(char *s)
+{
+    int lenght = 0;
+    int i;
+    int j;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        lenght++;
+    }
+    for (j = i; s[j]>=lenght; j--)
+    {
+        putchar(s[j]);
+    }
+    putchar('\n');
+}
