06a22ad7f8bcca6ed1c15429c2dc5ee7f3dfa3d3
Abd03mg <eemgyg@gmail.com>
2023-07-19 09:50:02 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,12 @@
 /**
 * _isalpha - check is alpha
 * @c: the arg should be check
-* Retun: always 0
+* Return: always 0
 */
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90)||(c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
