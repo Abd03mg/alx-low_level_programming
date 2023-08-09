05d18347432a12551be06c56b19f04890e343f95
abdo <eemgyg@gmail.com>
2023-08-10 01:02:28 +0200

    modify 0-whats

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: return 0 if success.
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv[0]);
+	printf("%s\n", *argv);
 
 	return (0);
 }
