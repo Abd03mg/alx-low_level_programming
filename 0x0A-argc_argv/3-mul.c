3bb927541aed1d2792bbb1de2dd9bca1d87ffb84
abdo <eemgyg@gmail.com>
2023-08-10 02:04:36 +0200

    push 3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that multiplies two numbers.
+ * @argc: count of arguments.
+ * @argv: value of arguments.
+ * Return: return 0 if success, 1 if faild.
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d", argv[argc - 1] * argv[argc - 2]);
+
+	return (0);
+
+}
+
