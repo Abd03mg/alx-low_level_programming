eb706336eca8e1db3d1cd0a3226a309e8a3d386b
abdo <eemgyg@gmail.com>
2023-08-15 01:30:55 +0200

    push dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef FILE_H
+#define FILE_H
+
+/**
+ * struct dog - short Description.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
