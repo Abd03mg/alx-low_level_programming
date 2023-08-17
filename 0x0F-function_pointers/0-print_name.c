4ee8f6b54e6fa852b4cb0fe2879a49d7d112585d
abdo <eemgyg@gmail.com>
2023-08-17 12:42:17 +0200

    push readme, header and print_name files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that print a name.
+ * @name: the name will be printed.
+ * @f: pointer to function.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(*name);
+}
