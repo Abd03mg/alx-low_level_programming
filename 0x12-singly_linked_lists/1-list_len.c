068f3ef33f24d103c3af5ffff4d13e9fabd81ead
abdo <eemgyg@gmail.com>
2023-08-25 19:37:37 +0200

    add list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists"
+
+/**
+ * list_len - function that returns the number of elements.
+ * @h: input liked list.
+ * Return: number of elements.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elems = 0;
+
+	while (h)
+	{
+		h = h->next;
+		elems++;
+	}
+	return (elems);
+}
