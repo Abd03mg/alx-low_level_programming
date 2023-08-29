4a5b6e36b0b76f7bf752471ab10dca44e30989a9
abdo <eemgyg@gmail.com>
2023-08-30 01:43:40 +0200

    push free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that free linked list
+ * and set head to null.
+ * @head: double pointer to struct.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *swap;
+
+	if (*head == NULL)
+		return (NULL);
+
+	while (*head)
+	{
+		swap = *head->next;
+		free(*head);
+		*head = swap;
+	}
+	*head = NULL;
+}
