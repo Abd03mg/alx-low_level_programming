47ca483566a320cdea52dfce745b672f41e3b95c
abdo <eemgyg@gmail.com>
2023-08-31 01:46:08 +0200

    push delete nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *cp = *head;
 	listint_t *next;
 
-	if (!head || !(*head))
+	if (!head)
 		return (-1);
 
 	if (index == 0)
