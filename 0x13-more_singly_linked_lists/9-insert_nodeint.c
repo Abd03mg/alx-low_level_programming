3cf2aaa3f51849a85e99fc9956503ce9ba1dc2f3
abdo <eemgyg@gmail.com>
2023-08-30 21:02:11 +0200

    push isert nodeint at end

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,8 +20,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	if (idx == 0)
 	{
-		new->next = cp->next;
-		cp->next = new;
+		new->next = *head;
+		*head = new;
 		return (new);
 	}
 
