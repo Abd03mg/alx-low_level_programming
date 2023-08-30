a3f0fb1415c126db81eb270d54f6e86fcc9a7d80
abdo <eemgyg@gmail.com>
2023-08-30 04:25:56 +0200

    push get_nodeint

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (counter = 1; counter <= index; counter++)
 	{
 		cp = cp->next;
-		if (index > counter)
+		if (!cp)
 			return (NULL);
 	}
 	
