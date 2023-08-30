2812ad976627c0ec6e1d9e5916b463df65485434
abdo <eemgyg@gmail.com>
2023-08-30 04:16:08 +0200

    push get_nodeint

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function that return node at nth index.
+ * @head: pointer to struct.
+ * @index: specific index of node must return.
+ * Return: node, 0 if node does not exist.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int counter;
+	listint_t *cp = head;
+
+	if (!head)
+		return (0);
+
+	for (counter = 1; counter <= index; counter++)
+		cp = cp->next;
+	return (cp);
+}
