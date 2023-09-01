64f30e3562745ffd610fbb17c1f6e00d4b3b83df
abdo <eemgyg@gmail.com>
2023-09-01 06:10:57 +0200

    delete folders

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILE_H
-#define FILE_H
-
-#define ABS(x) ((x) < 0 ? (-1 * (x)) : (x))
-
-#endif
--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNC_H
-#define FUNC_H
-#include <stdarg.h>
-
-int sum_them_all(const unsigned int n, ...);
-
-#endif
