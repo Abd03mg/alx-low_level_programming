07b9fdd2effcc39d8b576f8a4be3b1efe17476c7
Abdoemg <eemgyg@gmail.com>
2023-10-07 19:01:44 +0200

    push 0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - program that print its name.
+ * @argc: number of arguments.
+ * @argv: program comilintation argument.
+ * Return: 0 if success.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("/s\n", *argv);
+	return (0);
+}
