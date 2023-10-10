b372457dd9412585cf928c3fad833b20eddbce48
Abdoemg <eemgyg@gmail.com>
2023-10-10 21:46:19 +0200

    push dog header

---
+++ 0x0E-structers_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structcer for dog.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
