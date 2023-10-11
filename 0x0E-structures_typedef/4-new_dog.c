443131248cdae0212aff3ea611475d6cc1a78568
abdo <eemgyg@gmail.com>
2023-10-11 03:23:30 +0200

    push 4-new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,21 @@
 #include "dog.h"
+/**
+ * _strcpy - function that return copy of string.
+ * @dest: destination strinng.
+ * @src: source strig.
+ * Return: copy of string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0, i;
 
+	while (src[len] != '\0')
+		len++;
+	for (i = 0; i < len; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
 /**
  * new_dog - function that creates new dog.
  * @name: new dog name.
@@ -11,16 +27,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
+	int nlen = 0, olen = 0;
+
+	while (name[nlen] != '\0')
+		nlen++;
+	while (owner[olen] != '\0')
+		olen++;
 
 	new = malloc(sizeof(dog_t));
-	new->name = malloc(sizeof(name) + 1);
-	new->owner = malloc(sizeof(owner) + 1);
+	new->name = malloc(nlen + 1);
+	new->owner = malloc(olen + 1);
 	if (!(new || new->name || new->owner))
 		return (NULL);
 
-	strcpy(new->name, name);
+	_strcpy(new->name, name);
 	new->age = age;
-	strcpy(new->owner, owner);
+	_strcpy(new->owner, owner);
 
 	return (new);
 }
