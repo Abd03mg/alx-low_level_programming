7c4d40e8a567f78977cbaf721cbb32963ba9ef9e
abdo <eemgyg@gmail.com>
2023-10-11 02:11:52 +0200

    correct print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,10 +8,10 @@
 void print_dog(struct dog *d)
 {
 	if (!d)
-		exit(0);
+		return;
 	if (d)
 	{
-		if (!(d->name))
+		if ((d->name) == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
