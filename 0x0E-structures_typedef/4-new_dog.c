268184cee64a1da8ab9425acd020ccc60ba32780
abdo <eemgyg@gmail.com>
2023-10-11 02:54:48 +0200

    push 4-new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * new_dog - function that creates new dog.
+ * @name: new dog name.
+ * @age: new dog age.
+ * @owner: new dog owner.
+ * Return: new dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
+
+	return (new);
+}
