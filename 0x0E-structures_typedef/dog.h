05c8b6df24139205f9390ff4a2772357162f3804
abdo <eemgyg@gmail.com>
2023-10-11 03:32:09 +0200

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * struct dog - structcer for dog.
  * @name: name of dog.
@@ -20,5 +21,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
-
+char *_strcpy(char *dest, char *src);
 #endif
