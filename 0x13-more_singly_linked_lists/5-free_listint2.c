97735194de470ed1d5f7701f454d8bf24b7bb56f
abdo <eemgyg@gmail.com>
2023-10-24 22:38:24 +0200

    free list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - fuction that frees a list.
+ *
+ * @head: listint_t list.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
+}
