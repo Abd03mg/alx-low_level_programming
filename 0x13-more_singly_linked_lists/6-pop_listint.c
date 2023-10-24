b1d4fad5dfe746f310eb7c6260680331d9e0eb2c
abdo <eemgyg@gmail.com>
2023-10-24 22:56:56 +0200

    pop head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that pops head node of list.
+ *
+ * @head: listint_t list.
+ * Return: poped node data.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp = *head;
+	int t = tmp->n;
+
+	tmp = tmp->next;
+	free(*head);
+	*head = tmp;
+	return (t);
+}
