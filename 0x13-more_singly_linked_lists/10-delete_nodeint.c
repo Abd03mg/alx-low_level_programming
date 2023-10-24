d097d94af450bfe242c5524bb31cb22296d01a8b
abdo <eemgyg@gmail.com>
2023-10-25 00:02:06 +0200

    delete at idx

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - functio that deletes node at idx.
+ *
+ * @head: listint_t list.
+ * @index: index of node to be deleted.
+ * Return: 1 if success or -1 if it fails.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *tmp = *head, *new = NULL;
+
+	while (i < index - 1)
+	{
+		if (!tmp || !(tmp->next))
+			return (-1);
+		tmp = tmp->next;
+		i++;
+	}
+	new = tmp->next;
+	tmp->next = new->next;
+	free(new);
+	return (1);
+}
