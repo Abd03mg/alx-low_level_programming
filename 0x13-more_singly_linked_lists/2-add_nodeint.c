da45a08cdddf32cee8553b01857df46fa7336ad2
abdo <eemgyg@gmail.com>
2023-10-24 13:57:04 +0200

    betty

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - function that return new node at 
+ * add_nodeint - function that return new node at
  * linked list.
  *
  * @head: double poinnter to list.
