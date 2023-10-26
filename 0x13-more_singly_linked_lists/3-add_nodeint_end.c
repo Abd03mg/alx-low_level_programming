8beec9070cb2f46aa64daf50e510928ab8878f71
abdo <eemgyg@gmail.com>
2023-10-26 23:26:58 +0200

    edit folder ame

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,7 +11,7 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *new, **tmp = head;
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
@@ -19,8 +19,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->n = n;
 	new->next = NULL;
 
-	for ( ; *head; )
-		head = &(*head)->next;
-	*head = new;
-	return (*head);
+	for ( ; *tmp; )
+		*tmp = (*tmp)->next;
+	(tmp) = &new;
+	return (*tmp);
 }
