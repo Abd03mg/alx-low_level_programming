d6e1bcdaf1184127e65590beaf015a726ded0ec0
Abdoemg <eemgyg@gmail.com>
2023-11-04 00:37:05 +0200

    fix err

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, i;
 
-	if (!filename || text_content == NULL)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
