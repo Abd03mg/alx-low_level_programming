6802b6525ca5056087686be8a068e40b82b87807
Abd03mg <eemgyg@gmail.com>
2023-12-07 09:37:52 -0800

    end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (!new)
 		return (NULL);
 
-	for ( ; (*head); )
+	for ( ; (*head)->next; )
 		head = &(*head)->next;
 
 	new->n = n;
