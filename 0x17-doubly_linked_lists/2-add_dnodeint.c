a0f76adc2147e38a1346b9762a707f03e504c12a
Abd03mg <eemgyg@gmail.com>
2023-12-07 06:50:54 -0800

    add node at beg

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function that adds new node at beginning of list.
+ *
+ * @head: first node at list.
+ * @n: value of new node.
+ * Return: new element of NULL if it failed.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
