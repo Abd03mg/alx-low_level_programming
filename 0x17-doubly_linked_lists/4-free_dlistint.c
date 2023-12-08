34a502c97e7d77813b6f736fc9246baac3c44971
Abdoemg <eemgyg@gmail.com>
2023-12-08 17:53:07 +0200

    free

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,12 +8,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp = head;
+	dlistint_t *tmp;
 
 	while (head)
 	{
+		tmp = head->next;
 		free(head);
-		head = tmp->next;
+		head = tmp;
 	}
-	free(tmp);
 }
