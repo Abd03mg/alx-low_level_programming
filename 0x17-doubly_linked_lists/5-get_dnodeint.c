65979672b057882b8984aebd89b3ea538ee29a93
Abdoemg <eemgyg@gmail.com>
2023-12-08 17:59:05 +0200

    nth idx

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function that returns the nth node.
+ *
+ * @head: pointer to dlistint list.
+ * @index: index of needed node.
+ * Return: node.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+		head = head->next;
+	return (head);
+}
